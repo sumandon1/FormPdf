68ac65d2fab42e690b535ca80adf93434ed65beb
Suman <165036335+sumandon1@users.noreply.github.com>
2024-08-02 18:48:43 +0530

    Add files via upload

---
+++ k5_html.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+<title>Html code</title>
+<style>
+body{
+color:dark;
+background-color:black;
+}
+table{
+
+background-color:grey;}
+</style>
+</head>
+
+<body>
+<body bg-color="blue">
+<form action="k5_backend.php" method="POST">
+<table border="25" align="center" style="margin-top:100px;">
+<tr>
+<td style="background-color:dark;color:pink"align="center"><b>Student Details:-</b><br></td></tr>
+<tr><td><input type="text" style="color:red" placeholder="Students Name"name="sn"></input><br></td></tr>
+<tr><td><input type="text" style="color:red" placeholder="Fateher's Name"name="fn"></input><br></td></tr>
+<tr><td><input type="text" style="color:red" placeholder="Mother's Name"name="mn"></input><br></td></tr>
+</table>
+<div align="center">
+<input type="submit"style="margin-top:10px;color:pink;background-color:grey" placeholder=""value="submit" name="submit"></input>
+</div>
+</body>
+
+
+
+</html>
